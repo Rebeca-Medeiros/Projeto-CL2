73b6c1e4f4f71cd6407d6080e7e381c4936abd46
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-23 15:09:41 -0200

    Update CI_SmartLift.v
    inputs e outputs

--- Code/CI_SmartLift.v
+++ Code/CI_SmartLift.v
@@ -1,1 +1,7 @@
-
+module CI_SmartLift(/*clk, res,*/ sw, LED_RED, LED_GREEN, HEX0, HEX1);
+	input [8:0]sw; //chaves responsáveis pela seleção dos andares
+	output reg [3:0] LED_GREEN; //led verde para quando as portas estiverem abertas
+	output reg [3:0] LED_RED; //led vermelho para quando as portas estiverem fechadas
+	output [6:0] HEX0; //primeiro display, vai mostrar o andar escolhido pelas chaves
+	output [6:0] HEX1; //segundo display, vai mostar o movimento do elevador
+  
