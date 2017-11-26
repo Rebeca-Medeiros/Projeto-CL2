f94846150d3dee2cc62c2469585c95067b230ffe
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-25 23:33:19 -0200

    Update SmartLift.v

--- Code/SmartLift.v
+++ Code/SmartLift.v
@@ -1,193 +1,188 @@
-module SmartLift(SW, LED_G, LED_R, HX0, HX1 /*clock, reset*/ )
-	input [8:0]SW;
-	output reg LED_G;
-	output reg LED_R;
-	output reg [6:0]HX0;
-	output reg [6:0]HX1;
+module smartlift(SW, LED_G, LED_R, HEX0, HEX1, KEY0, CLOCK_50);
 	
-	reg count;
+	input [8:0]SW; 		//andar selecionado
+	input KEY0; 			//solicitar o andar
+	input CLOCK_50;		
+	output reg LED_G;	//porta aberta
+	output reg LED_R;	//porta fechada
+	output reg[6:0]HEX0 = ~7'h06;	//print andar selecionado
+	output reg[6:0]HEX1 = ~7'h06;	//andar atual
 	
-	assign HX0 = ( SW[8:0] == 9'b000000000)? 7'b0111111:// 0
-				 ( SW[8:0] == 9'b000000001)? 7'b0000110:// 1
-		         ( SW[8:0] == 9'b000000010)? 7'b1011011:// 2
-		         ( SW[8:0] == 9'b000000100)? 7'b0101111:// 3
-		         ( SW[8:0] == 9'b000001000)? 7'b1100110:// 4
-		         ( SW[8:0] == 9'b000010000)? 7'b1101101:// 5
-		         ( SW[8:0] == 9'b000100000)? 7'b1111100:// 6
-		         ( SW[8:0] == 9'b001000000)? 7'b0000111:// 7
-		         ( SW[8:0] == 9'b010000000)? 7'b1111111:// 8
-		         :7b'0000000
-		         ;
-		   
-	assign HX1 = ( count == 9'b000000000) ? 7'b0111111:// 0
-				 ( count == 9'b000000001)? 7'b0000110:// 1
-		         ( count == 9'b000000010)? 7'b1011011:// 2
-		         ( count == 9'b000000100)? 7'b0101111:// 3
-		         ( count == 9'b000001000)? 7'b1100110:// 4
-		         ( count == 9'b000010000)? 7'b1101101:// 5
-		         ( count == 9'b000100000)? 7'b1111100:// 6
-		         ( count == 9'b001000000)? 7'b0000111:// 7
-		         ( count == 9'b010000000)? 7'b1111111:// 8
-				 :7b'00000000;
+	//seg - (g, f, e, d, c, b, a)
 	
+	integer movimento; // 0 - parado, 1 - subindo, 2 - descendo 
+	integer s = 9; //andar solicitado
+	reg [1:0] estado_atual, estado_anterior;
+	parameter andar0 = 0, andar1 = 1, andar2 = 2, andar3 = 3, andar4 = 4, andar5 = 5, andar6 = 6, andar7 = 7, andar8 = 8;
 	
-	
-	always @(*)begin
-		case(SW[8:0])
+	always @( negedge KEY0 ) begin 
+		case (SW) 	
+						9'b000000001: begin
+							HEX0 = ~7'h06; //se o andar selecionado for 0
+							s = 0;
+						end	
+						
+						9'b000000010: begin
+							HEX0 = 7'b0000110; //se o andar selecionado for 1
+							s = 1;
+						end
+						
+						9'b000000100: begin
+							HEX0 = 7'b1011011; //se o andar selecionado for 2
+							s = 2;
+						end
+						
+						9'b000001000: begin
+							HEX0 = 7'b1001111; //se o andar selecionado for 3
+							s = 3;
+						end	
+						
+						9'b000010000: begin
+							HEX0 = 7'b1100110; //se o andar selecionado for 4
+							s = 4;
+						end
+						
+						9'b000100000: begin
+							HEX0 = 7'b1101101; //se o andar selecionado for 5
+							s = 5;
+						end
+						
+						9'b001000000: begin
+							HEX0 = 7'b1111100; //se o andar selecionado for 6
+							s = 6;
+						end
+						
+						9'b010000000: begin 
+							HEX0 = 7'b0000111; //se o andar selecionado for 7
+							s = 7;
+						end
+						/*
+						9'b100000000: begin 
+							HEX0 = 7'b1111111;                                                                                                                                                                                                                        1; //se o andar selecionado for 8
+							s = 8;
+						end */
+						
+						default: begin 
+							HEX0 = 7'b0001000;  //default: _ , nenhuma andar solicitado
+							s = 9; // indefinido
+						end 
+			endcase
+	end
+
+
+	always @ (*) begin //parte combinacional 	
+		case (estado_atual)
+			andar0: begin
+				HEX1 = 7'b0111111; //andar atual = 0
+			end
 			
-				9'b000000000: begin
-				count <= 9'b000000000;
-				LED_G[0] = 1'b1;
-				LED_G[1] = 1'b1;		
-				LED_G[2] = 1'b1;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b0;
-				LED_R[5] = 1'b0;
-				LED_R[6] = 1'b0;
-				LED_R[7] = 1'b0;
-				end
-				
-				9'b000000001: begin
-				count <= 9'b000000001;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				9'b000000010: begin
-				count <= 9'b000000010;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				
-				9'b000000011: begin
-				count <= 9'b000000100;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				9'b000000100: begin
-				count <= 9'b000001000;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				9'b000000101: begin
-				count <= 9'b000010000;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				9'b000000110: begin
-				count <= 9'b000010000;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				9'b000001000: begin
-				count <= 9'b000100000;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				
-				9'b000001000: begin
-				count <= 9'b010000000;
-				LED_G[0] = 1'b0;
-				LED_G[1] = 1'b0;		
-				LED_G[2] = 1'b0;
-				LED_G[3] = 1'b1;
-				LED_R[4] = 1'b1;
-				LED_R[5] = 1'b1;
-				LED_R[6] = 1'b1;
-				LED_R[7] = 1'b1;
-				end
-				
-				
-				
-				endcase
-				
-				
+			andar1: begin 
+				HEX1 = 7'b0000110; //andar atual = 1
+			end
 			
+			andar2: begin 
+				HEX1 = 7'b1011011; //andar atual = 2
+			end
+			
+			andar3: begin 
+				HEX1 = 7'b1001111; //andar atual = 3
+			end
+			
+			andar4: begin 
+				HEX1 = 7'b1100110; //andar atual = 4
+			end
+			
+			andar5: begin 
+				HEX1 = 7'b1101101; //andar atual = 5
+			end
+			
+			andar6: begin 
+				HEX1 = 7'b1111100; //andar atual = 6
+			end
+			
+			andar7: begin 
+				HEX1 = 7'b0000111; //andar atual = 7
+			end
+			
+			andar8: begin 
+				HEX1 = 7'b1111111; //andar atual = 8
+			end
+		endcase
+	end
+	
+	always @( posedge CLOCK_50 ) begin //parte sequencial 
+		
+		case (estado_atual) 
+			
+			andar0: begin
+				if ( (s > 0) && (s < 9) ) begin
+					estado_atual <= andar1;
+					estado_anterior <= andar0; 
+				end
+			end
+			
+			andar1: begin
+				if ( (s > 1) && (s < 9) ) begin
+					estado_atual <= andar2;
+					estado_anterior <= andar1; 
+				end
+			end
+			
+			andar2: begin
+				if ( (s > 2) && (s < 9) ) begin
+					estado_atual <= andar3;
+					estado_anterior <= andar2; 
+				end
+			end
+			
+			andar3: begin
+				if( (s > 3) && (s < 9) ) begin
+					estado_atual <= andar4;
+					estado_anterior <= andar3;
+				end
+			end
+			
+			andar4: begin
+				if( (s > 4) && (s < 9) ) begin
+					estado_atual <= andar5;
+					estado_anterior <= andar4;
+				end
+			end
+			
+			andar5: begin
+				if( (s > 5) && (s < 9) ) begin
+					estado_atual <= andar6;
+					estado_anterior <= andar5;
+				end
+			end
+			
+			andar6: begin
+				if( (s > 6) && (s < 9) ) begin
+					estado_atual <= andar7;
+					estado_anterior <= andar6;
+				end
+			end
+			
+			andar7: begin
+				if( (s > 7) && (s < 9) ) begin
+					estado_atual <= andar8;
+					estado_anterior <= andar7;
+				end
+			end
+			
+			/*andar8: begin
+				if( (s > 8) && (s < 9) ) begin
+					estado_atual <= andar8;
+					estado_anterior <= andar8;
+				end
+			end*/
 				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+			endcase
+
+	end				
+	
+endmodule
 				
 					
 				
-				
+		
