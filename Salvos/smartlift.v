fbfcc7b9d366b5ba6bb693d213a33181eabd7c33
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-11-28 16:07:44 -0200

    Update smartlift.v

--- Salvos/smartlift.v
+++ Salvos/smartlift.v
@@ -1,6 +1,7 @@
 module smartlift(SW, LED_G, LED_R, HEX0, HEX1, KEY0, CLOCK_50, LCD_EN, LCD_DATA, 
 				 LCD_EN, LCD_RS, LCD_RW);
 	//input LCD, todas as entradas do lcd
+	//input 
 	output	[7:0]	LCD_DATA; //entrada do lcd
 	output	LCD_RW, LCD_EN, LCD_RS; //saída do lcd
 	input [8:0]SW; 		//andar selecionado
@@ -48,6 +49,204 @@ module smartlift(SW, LED_G, LED_R, HEX0, HEX1, KEY0, CLOCK_50, LCD_EN, LCD_DATA,
 							HEX0 = 7'b0110000; //se o andar selecionado for 3
 							s = 3;
 						end	
+						
+						9'b000010000: begin
+							HEX0 = 7'b0011001; //se o andar selecionado for 4
+							s = 4;
+						end
+						
+						9'b000100000: begin
+							HEX0 = 7'b0010010; //se o andar selecionado for 5
+							s = 5;
+						end
+						
+						9'b001000000: begin
+							HEX0 = 7'b0000010; //se o andar selecionado for 6
+							s = 6;
+						end
+						
+						9'b010000000: begin 
+							HEX0 = 7'b1111000; //se o andar selecionado for 7
+							s = 7;
+						end
+						
+						9'b100000000: begin 
+							HEX0 = 7'b0000000; //se o andar selecionado for 8                                                                                                                                                                                                              1; //se o andar selecionado for 8
+							s = 8;
+						end
+						
+						default: begin 
+							HEX0 = 7'b1110111;  //default: _ , nenhuma andar solicitado
+						end 
+			endcase
+	end
+
+
+	always @ (*) begin //parte combinacional 	
+		case (estado_atual)
+			andar0: begin
+				HEX1 = 7'b1000000; //andar atual = 0
+			end
+			
+			andar1: begin 
+				HEX1 = 7'b1111001; //andar atual = 1
+			end
+			
+			andar2: begin 
+				HEX1 = 7'b0100100; //andar atual = 2
+			end
+			
+			andar3: begin 
+				HEX1 = 7'b0110000; //andar atual = 3
+			end
+			
+			andar4: begin 
+				HEX1 = 7'b0011001; //andar atual = 4
+			end
+			
+			andar5: begin 
+				HEX1 = 7'b0010010; //andar atual = 5
+			end
+			
+			andar6: begin 
+				HEX1 = 7'b0000010; //andar atual = 6
+			end
+			
+			andar7: begin 
+				HEX1 = 7'b1111000; //andar atual = 7
+			end
+			
+			andar8: begin 
+				HEX1 = 7'b00000000; //andar atual = 8
+			end
+		endcase
+	end
+	
+	always @( posedge CLOCK_50 ) begin //parte sequencial 
+		
+		case (estado_atual) 
+			
+			andar0: begin
+				if ( s > 0 ) begin
+					estado_atual <= andar1;
+					estado_anterior <= andar0; 
+					//movimento = 1;
+				end
+			end
+			
+			andar1: begin
+				if ( s > 1 ) begin
+					estado_atual <= andar2;
+					estado_anterior <= andar1; 
+					//movimento = 1;
+				end
+				if ( s < 1 ) begin
+					estado_atual <= andar0;
+					estado_anterior <= andar1;
+					//movimento = 2;					
+				end	
+			end
+			
+			andar2: begin
+				if ( s > 2 ) begin
+					estado_atual <= andar3;
+					estado_anterior <= andar2; 
+					//movimento = 1;
+				end
+				if ( s < 2 ) begin
+					estado_atual <= andar1;
+					estado_anterior <= andar2;
+					//movimento = 1;
+				end
+			end
+			
+			andar3: begin
+				if ( s > 3 ) begin
+					estado_atual <= andar4;
+					estado_anterior <= andar3;
+					//movimento = 1;
+				end
+				if ( s < 3 ) begin
+					estado_atual <= andar2;
+					estado_anterior <= andar3;
+					//movimento = 2;
+				end
+			end
+			
+			andar4: begin
+				if ( s > 4 ) begin
+					estado_atual <= andar5;
+					estado_anterior <= andar4; 
+					//movimento = 1;
+				end
+				if ( s < 4 ) begin
+					estado_atual <= andar3;
+					estado_anterior <= andar4;
+					//movimento = 2;
+				end
+			end
+			
+			andar5: begin
+				if ( s > 5 ) begin
+					estado_atual <= andar6;
+					estado_anterior <= andar5; 
+					//movimento = 1;
+				end
+				if ( s < 5 ) begin
+					estado_atual <= andar4;
+					estado_anterior <= andar5;
+					//movimento = 2;
+				end
+			end
+			
+			andar6: begin
+				if ( s > 6 ) begin
+					estado_atual <= andar7;
+					estado_anterior <= andar6;
+					//movimento = 1; 
+				end
+				if ( s < 6 ) begin
+					estado_atual <= andar5;
+					estado_anterior <= andar6;
+					//movimento = 2;				
+				end
+			end
+			
+			andar7: begin
+				if ( s > 7 ) begin
+					estado_atual <= andar8;
+					estado_anterior <= andar7; 
+					//movimento = 1;
+				end
+				if ( s < 7 ) begin
+					estado_atual <= andar6;
+					estado_anterior <= andar7;
+					//movimento = 2;
+				end
+			end
+			
+			andar8: begin
+				if ( s < 8 ) begin
+					estado_atual <= andar7;
+					estado_anterior <= andar8;
+					//movimento = 0; a
+				end
+				if ( s < 8 ) begin
+					estado_atual <= andar7;
+					estado_anterior <= andar8;
+					//movimento = 1;
+				end
+			end
+			
+			endcase
+			
+	end				
+	
+endmodule
+				
+					
+				
+		
 						
 						9'b000010000: begin
 							HEX0 = 7'b0011001; //se o andar selecionado for 4
